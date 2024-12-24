9d3364ca150289ded416e233fd65aacfca4255e8
Pratiksha Nikam <pratiksha.gawande@gmail.com>
2024-12-24 17:29:04 +0530

    debugging

--- medify/src/components/Calender/TimeSlotPicker/TimeSlotPicker.jsx
+++ medify/src/components/Calender/TimeSlotPicker/TimeSlotPicker.jsx
@@ -1,4 +1,4 @@
-import styled from "@emotion/styled";
+//import styled from "@emotion/styled";
 import { Chip, Stack, Typography, Divider } from "@mui/material";
 
 export default function TimeSlotPicker({
